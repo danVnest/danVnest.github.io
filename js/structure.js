c514370b994499026d6d15ff122a3d56d7cda744
Daniel Vogelnest <daniel.vogelnest@me.com>
2014-05-15 01:18:45 +0000

    moved to node.js, added basic project list and animations to the navigation bar

--- js/structure.js
+++ js/structure.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 	var root = $('html, body');
 	var header = $('header');
+	var projectList = $('#project-list');
 	var nav = $('nav');
 	var dockHeight = $(window).innerHeight() - nav.height();
+	var projectHeight = $(document).height();
 	header.css('height', dockHeight);
-	var docked = false;
+	var section = 0;
 	var docking = false;
 	var searchWidthInit = $('#search').innerWidth();
 	if (!window.getComputedStyle(document.querySelector('.sticky')).position.match('sticky')){
@@ -12,7 +14,7 @@ $(document).ready(function(){
 		docking = true;
 	}
 	$(window).scroll(function(){
-		if (!docked && ($(window).scrollTop() >= dockHeight)){
+		/*if (!docked && ($(window).scrollTop() >= dockHeight)){
 			if (docking) nav.addClass('docked');
 			$('#sectionLabel').text('Projects');
 			$('#switch svg').transition({ perspective: '100px', rotateX: '180deg' });
@@ -24,6 +26,33 @@ $(document).ready(function(){
 			$('#switch svg').transition({ perspective: '100px', rotateX: '0deg' });
 			docked = false;
 		}
+		*/
+		if ($(window).scrollTop() < dockHeight) {
+			if (section != 0) {
+				if (docking) nav.removeClass('docked');
+				$('#sectionLabel').text('Daniel Vogelnest');
+				$('#switch svg').transition({ perspective: '100px', rotateX: '0deg' });
+				section = 0;
+			}
+		} 
+		else if ($(window).scrollTop() < projectHeight) {
+			if (section != 1) {
+				$('#sectionLabel').text('Projects');
+				if (section == 0) {
+					if (docking) nav.addClass('docked');
+					$('#switch svg').transition({ perspective: '100px', rotateX: '180deg' });
+				}
+				else {
+					$('#switch svg').transition({ perspective: '100px', rotateX: '180deg' }, 1000);
+				}
+				section = 1;
+			}
+		}
+		else if (section != 2) {
+			$('#sectionLabel').text('Building a Website');
+			$('#switch svg').transition({ perspective: '100px', rotateX: '540deg' }, 1000);
+			section = 2;
+		}
 	});
 	$(window).resize(function(){
 		dockHeight = $(window).innerHeight() - nav.height();
@@ -31,11 +60,16 @@ $(document).ready(function(){
 	});
 	$('#switch').click(function(e){
 		e.preventDefault();
-		if (docked) {
+		if (section == 1) {
 			root.animate({ scrollTop: 0 }, 500);
-		} else {
+		}
+		else {
 			root.animate({ scrollTop: dockHeight }, 500);
 		}
+	});
+	$('#temp-project-brief').click(function(e){
+		e.preventDefault();
+		$('#project').fadeIn();
 	});
 	$('#searchBox').focusin(function() {
 		root.animate({ scrollTop: dockHeight }, 500);
